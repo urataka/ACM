14f02cc9f914aa7492f49142d7b26db74550801b
Nobumasa <nobumasa@nobumasa-no-MacBook-Air.local>
2013-04-13 22:49:22 +0900

    railsプロジェクト作成

---
+++ acm/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Acm::Application.config.secret_key_base = '8368a931435106b57f170cc58b2d91c89fccfaed0d33681068527fe20fdbec317ecbd8165627981149aa59ef9ca271296ecbe37f9f7f6f4ce53b71ff51039473'
